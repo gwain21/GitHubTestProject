9bb954ac963018f2f70b3593b43b1465b4e81698
gwain21 <keegans.myth@yahoo.com>
2017-07-03 15:15:47 -0400

    Initial Commit

---
+++ app/src/main/java/com/smithkeegan/githubtestproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.smithkeegan.githubtestproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((TextView)findViewById(R.id.main_textView)).setText("This is text");
+    }
+}

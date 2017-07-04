5180b26edf888f0e4c458930e65ec79d75735f1c
gwain21 <keegans.myth@yahoo.com>
2017-07-03 20:33:35 -0400

    Commit five

--- app/src/main/java/com/smithkeegan/githubtestproject/MainActivity.java
+++ app/src/main/java/com/smithkeegan/githubtestproject/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Ayyy boy, commit five
         ((TextView)findViewById(R.id.main_textView)).setText("This is text");
         //New comment here, commit three
         //New comment as well, commit four

90f2180494896f61ad300b75b90ea7f702938320
gwain21 <keegans.myth@yahoo.com>
2017-07-03 21:24:09 -0400

    Commit nine, to be pulled by mac.

--- app/src/main/java/com/smithkeegan/githubtestproject/MainActivity.java
+++ app/src/main/java/com/smithkeegan/githubtestproject/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         //Adding a new branch, commit six
         //Adding a newer branch! Commit seven, merge then push.
         //Commit eight, some changes.
+        //Commit nine. To be placed in remote branch and pulled from mac.
     }
 }

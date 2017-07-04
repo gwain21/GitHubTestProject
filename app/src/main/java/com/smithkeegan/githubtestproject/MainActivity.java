e0535d03f52bff8ab7675b147ade7e9c329bf0e6
gwain21 <keegans.myth@yahoo.com>
2017-07-03 20:30:17 -0400

    Commit three

--- app/src/main/java/com/smithkeegan/githubtestproject/MainActivity.java
+++ app/src/main/java/com/smithkeegan/githubtestproject/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((TextView)findViewById(R.id.main_textView)).setText("This is text");
+        //New comment here, commit three
     }
 }

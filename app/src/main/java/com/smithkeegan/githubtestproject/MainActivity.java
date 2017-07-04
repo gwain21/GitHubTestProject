d32721a7357c04f09671b23b681491bac8492076
Keegan Smith <keegan.smith@willowtreeapps.com>
2017-07-03 22:53:20 -0400

    Commit to branch-three-develop

--- app/src/main/java/com/smithkeegan/githubtestproject/MainActivity.java
+++ app/src/main/java/com/smithkeegan/githubtestproject/MainActivity.java
@@ -18,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
         //Adding a newer branch! Commit seven, merge then push.
         //Commit eight, some changes.
         //Commit nine. To be placed in remote branch and pulled from mac.
+        //Commit 10, commit from mac, placed in branch-three-develop
     }
 }
